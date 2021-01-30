0ac3eb8d45d07438e58cc4f227430dcf31920c64
veronnah <yoo.veroon@gmail.com>
2021-01-30 21:10:11 +0100

    post-formats created

---
+++ post-templates/post-aside.php
@@ -0,0 +1,31 @@
+<article class="post">
+
+               <div class="entry-header cf">
+
+                  <h1><a href="<?php the_permalink(); ?>" title=""><?php the_title(); ?></a></h1>
+
+                  <p class="post-meta">
+
+                     <time class="date" datetime="2014-01-14T11:24">Jan 14, 2014</time>
+                     /
+                     <span class="categories">
+                     <a href="#">Design</a> /
+                     <a href="#">User Inferface</a> /
+                     <a href="#">Web Design</a>
+                     </span>
+
+                  </p>
+
+               </div>
+
+               <div class="post-thumb">
+                  <?php the_post_thumbnail(); ?>
+               </div>
+
+               <div class="post-content">
+                <?php the_post(); ?>
+                <?php the_content(); ?>
+
+               </div>
+
+            </article> <!-- post end -->
--- post-templates/post.php
+++ post-templates/post.php
@@ -1,1 +1,11 @@
-<h1><?php the_title(); ?></h1>
+<article class="post">
+
+   <div class="entry-header cf">
+      <h1><a href="<?php the_permalink(); ?>" title=""><?php the_title(); ?></a></h1>
+   </div>
+
+   <div class="post-content">
+    <?php the_content(); ?>
+   </div>
+
+</article> 
