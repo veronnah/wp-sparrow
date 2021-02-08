40302e01d8fa8474bd7f593c6c9413f437056436
veronnah <yoo.veroon@gmail.com>
2021-02-08 11:59:53 +0100

    end wp

--- portfolio-post.php
+++ portfolio-post.php
@@ -4,4 +4,162 @@ Template Name: Page with portfolio work
 Template Post Type: portfolio
 */
  ?>
- work
+
+<?php get_header(); ?>
+
+   <!-- Page Title
+   ================================================== -->
+   <div id="page-title">
+
+      <div class="row">
+
+         <div class="ten columns centered text-center">
+            <h1>Our Amazing Works<span>.</span></h1>
+
+            <p>Aenean condimentum, lacus sit amet luctus lobortis, dolores et quas molestias excepturi
+            enim tellus ultrices elit, amet consequat enim elit noneas sit amet luctu. </p>
+         </div>
+
+      </div>
+
+   </div> <!-- Page Title End-->
+
+   <!-- Content
+   ================================================== -->
+   <div class="content-outer">
+
+      <div id="page-content" class="row portfolio">
+
+         <section class="entry cf">
+
+            <div id="secondary"  class="four columns entry-details">
+
+                  <h1><?php the_title();?></h1>
+
+                  <div class="entry-description">
+
+                     <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                     nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate
+                     cursus a sit amet mauris. Morbi accumsan ipsum velit. Nam nec tellus a odio tincidunt auctor a
+                     ornare odio.</p>
+
+                  </div>
+
+                  <ul class="portfolio-meta-list">
+						   <li><span>Date: </span><?php the_field('project_date'); ?></li>
+						   <li><span>Client </span><?php the_field('client'); ?></li>
+						   <li><span>Skills: </span>Photoshop, Photography, Branding</li>
+				      </ul>
+
+                  <a class="button" href="http://behance.net">View project</a>
+
+            </div> <!-- secondary End-->
+
+            <div id="primary" class="eight columns">
+
+               <div class="entry-media">
+
+                  <img src="images/portfolio/entries/geometric-backgrounds-01.jpg" alt="" />
+
+                  <img src="images/portfolio/entries/geometric-backgrounds-02.jpg" alt="" />
+
+               </div>
+
+               <div class="entry-excerpt">
+
+				      <p>Proin gravida nibh vel velit auctor aliquet. Aenean sollicitudin, lorem quis bibendum auctor,
+                  nisi elit consequat ipsum, nec sagittis sem nibh id elit. Duis sed odio sit amet nibh vulputate
+                  cursus a sit amet mauris. Morbi accumsan ipsum velit. </p>
+
+					</div>
+
+            </div> <!-- primary end-->
+
+         </section> <!-- end section -->
+
+         <ul class="post-nav cf">
+			   <li class="prev"><a href="#" rel="prev"><strong>Previous Entry</strong> Duis Sed Odio Sit Amet Nibh Vulputate</a></li>
+				<li class="next"><a href="#" rel="next"><strong>Next Entry</strong> Morbi Elit Consequat Ipsum</a></li>
+			</ul>
+
+      </div>
+
+   </div> <!-- content End-->
+
+   <!-- Tweets Section
+   ================================================== -->
+   <section id="tweets">
+
+      <div class="row">
+
+         <div class="tweeter-icon align-center">
+            <i class="fa fa-twitter"></i>
+         </div>
+
+         <ul id="twitter" class="align-center">
+            <li>
+               <span>
+               This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+               Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
+               <a href="#">http://t.co/CGIrdxIlI3</a>
+               </span>
+               <b><a href="#">2 Days Ago</a></b>
+            </li>
+            <!--
+            <li>
+               <span>
+               This is Photoshop's version  of Lorem Ipsum. Proin gravida nibh vel velit auctor aliquet.
+               Aenean sollicitudin, lorem quis bibendum auctor, nisi elit consequat ipsum
+               <a href="#">http://t.co/CGIrdxIlI3</a>
+               </span>
+               <b><a href="#">3 Days Ago</a></b>
+            </li>
+            -->
+         </ul>
+
+         <p class="align-center"><a href="#" class="button">Follow us</a></p>
+
+      </div>
+
+   </section> <!-- Tweet Section End-->
+
+   <!-- footer
+   ================================================== -->
+   <footer>
+
+      <div class="row">
+
+         <div class="twelve columns">
+
+            <ul class="footer-nav">
+					<li><a href="#">Home.</a></li>
+              	<li><a href="#">Blog.</a></li>
+              	<li><a href="#">Portfolio.</a></li>
+              	<li><a href="#">About.</a></li>
+              	<li><a href="#">Contact.</a></li>
+               <li><a href="#">Features.</a></li>
+			   </ul>
+
+            <ul class="footer-social">
+               <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+               <li><a href="#"><i class="fa fa-twitter"></i></a></li>
+               <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+               <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+               <li><a href="#"><i class="fa fa-skype"></i></a></li>
+               <li><a href="#"><i class="fa fa-rss"></i></a></li>
+            </ul>
+
+            <ul class="copyright">
+               <li>Copyright &copy; 2014 Sparrow</li> 
+               <li>Design by <a href="http://www.styleshout.com/">Styleshout</a></li>               
+            </ul>
+
+         </div>
+
+         <div id="go-top" style="display: block;"><a title="Back to Top" href="#">Go To Top</a></div>
+
+      </div>
+
+   </footer> <!-- Footer End-->
+
+ <?php get_footer(); ?>
